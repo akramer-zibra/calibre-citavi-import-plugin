ccb4ba57aeb8e2f4cf6498397f9eea12035edd03
Achim <achim@zibra.de>
2020-08-26 20:12:05 +0200

    Rename package pages into steps

--- steps/filepick.py
+++ steps/filepick.py
@@ -5,33 +5,35 @@ from PyQt5.Qt import QWizardPage, QVBoxLayout, QLabel, QFileDialog, QDialog
 from calibre import prints
 from calibre.utils.monotonic import monotonic
 
-class FilepickPage(QWizardPage):
+class FilepickStep():
 
   __create_key = object()
 
   """ Protected constructor method """
   def __init__(self, create_key, parent, db):
-    assert(create_key == FilepickPage.__create_key), \
-        "FilepickPage objects must be created using FilepickPage.build"
+    assert(create_key == FilepickStep.__create_key), \
+        "FilepickStep objects must be created using FilepickStep.build"
     
     # Call super constructor
-    super(FilepickPage, self).__init__()
+    ## super(FilepickPage, self).__init__()
 
     # Dependency injection
     self.parent = parent
     self.db = db
 
+    """
     # Content
     self.l = QVBoxLayout()
     self.setLayout(self.l)
 
     self.label = QLabel('Wählen Sie hier die zu importierende BibTeX-Datei aus')
     self.l.addWidget(self.label)
+    """
 
     # Defines Filepicker for import file
     self.import_file = QFileDialog()
     self.import_file.setNameFilter("Files (*.bib)") # Only allow .bib files to be loaded
-    self.l.addWidget(self.import_file)
+    ## self.l.addWidget(self.import_file)
 
     # Attach listener method
     ## self.import_file.urlSelected.connect(FilepickPage.process)
@@ -42,11 +44,11 @@ class FilepickPage(QWizardPage):
       self.process(str(filename[0]))      
 
     # Register FileDialog as field
-    self.registerField("filepick", self.import_file)
+    ## self.registerField("filepick", self.import_file)
 
   """ Method processes the selected import file """
   def process(self, url):
-    print(url)
+    prints(url)
 
   ''' Factory Method for this page '''
   @classmethod
---
+++ steps/importcheck.py
@@ -0,0 +1,7 @@
+
+from PyQt5.Qt import QDialog, QVBoxLayout
+
+class ImportcheckDialog(QDialog):
+
+  def __init__(self, gui):
+    QDialog.__init__(self, gui)
