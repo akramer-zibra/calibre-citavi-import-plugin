ef729bd5234856286026cfcb48e00f2a6edc764c
Achim <achim@zibra.de>
2020-08-26 20:14:30 +0200

    Remove deprecated code

--- steps/filepick.py
+++ steps/filepick.py
@@ -13,37 +13,18 @@ class FilepickStep():
     assert(create_key == FilepickStep.__create_key), \
         "FilepickStep objects must be created using FilepickStep.build"
     
-    # Call super constructor
-    ## super(FilepickPage, self).__init__()
-
     # Dependency injection
     self.parent = parent
     self.db = db
 
-    """
-    # Content
-    self.l = QVBoxLayout()
-    self.setLayout(self.l)
-
-    self.label = QLabel('Wählen Sie hier die zu importierende BibTeX-Datei aus')
-    self.l.addWidget(self.label)
-    """
-
     # Defines Filepicker for import file
     self.import_file = QFileDialog()
     self.import_file.setNameFilter("Files (*.bib)") # Only allow .bib files to be loaded
-    ## self.l.addWidget(self.import_file)
-
-    # Attach listener method
-    ## self.import_file.urlSelected.connect(FilepickPage.process)
 
     if self.import_file.exec_() == QDialog.Accepted:
       filename = self.import_file.selectedFiles()
     if filename:
       self.process(str(filename[0]))      
-
-    # Register FileDialog as field
-    ## self.registerField("filepick", self.import_file)
 
   """ Method processes the selected import file """
   def process(self, url):
